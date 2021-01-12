650483117e85b47a9591f8ed98cef5ffe45b2c76
taujiong <taujiong@outlook.com>
2021-01-12 17:32:15 +0800

    feat(Algorithm): LeetCode 2020.1.12 每日一题 项目管理 困难

--- Algorithm/LeetCode/T0123-best-time-to-buy-and-sell-stock-iii.js
+++ Algorithm/LeetCode/T0123-best-time-to-buy-and-sell-stock-iii.js
@@ -6,8 +6,10 @@
  */
 var maxProfit = function (prices) {
   const n = prices.length;
-  let buy1 = -prices[0], buy2 = -prices[0];
-  let sell1 = 0, sell2 = 0;
+  let buy1 = -prices[0],
+    buy2 = -prices[0];
+  let sell1 = 0,
+    sell2 = 0;
   for (let i = 1; i < n; i++) {
     buy1 = Math.max(buy1, -prices[i]);
     sell1 = Math.max(sell1, buy1 + prices[i]);
