93d9a7d7239907470a070fcd4a196569f79c8001
taujiong <taujiong@outlook.com>
2021-07-18 14:13:54 +0800

    feat(Algorithm): LeetCode T690 员工的重要性 简单

---
+++ Algorithm/LeetCode/T0690-employee-importance.js
@@ -0,0 +1,16 @@
+import { Employee } from "./TypeDefinition.js";
+
+/**
+ * https://leetcode-cn.com/problems/employee-importance/
+ * 简单，递归
+ * @param {Employee[]} employees
+ * @param {number} id
+ * @return {number}
+ */
+var GetImportance = function (employees, id) {
+  const employee = employees.find((e) => e.id === id);
+  if (employee.subordinates.length === 0) return employee.importance;
+
+  const reducer = (acc, cur) => acc + GetImportance(employees, cur);
+  return employee.subordinates.reduce(reducer, employee.importance);
+};
--- Algorithm/LeetCode/TypeDefinition.js
+++ Algorithm/LeetCode/TypeDefinition.js
@@ -9,15 +9,28 @@ export class ListNode {
   }
 }
 
-/**
- * @param {number} val
- * @param {TreeNode | null} left
- * @param {TreeNode | null} right
- */
 export class TreeNode {
+  /**
+   * @param {number} val
+   * @param {TreeNode | null} left
+   * @param {TreeNode | null} right
+   */
   constructor(val = 0, left = null, right = null) {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+}
+
+export class Employee {
+  /**
+   * @param {number} id
+   * @param {number} importance
+   * @param {number[]} subordinates
+   */
+  constructor(id, importance, subordinates) {
+    this.id = id;
+    this.importance = importance;
+    this.subordinates = subordinates;
   }
 }
