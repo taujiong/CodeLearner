79ac411e6df14d75c2806e10009e2e3faeedabda
taujiong <taujiong@outlook.com>
2021-07-18 14:13:52 +0800

    feat(Algorithm): LeetCode 2020.1.17 每日一题 缀点成线 简单

---
+++ Algorithm/LeetCode/T1232-check-if-it-is-a-straight-line.js
@@ -0,0 +1,24 @@
+/**
+ * https://leetcode-cn.com/problems/check-if-it-is-a-straight-line/
+ * 简单
+ * @param {number[][]} coordinates
+ * @return {boolean}
+ */
+var checkStraightLine = function (coordinates) {
+  const deltaX = coordinates[0][0],
+    deltaY = coordinates[0][1];
+  const n = coordinates.length;
+  for (let i = 0; i < n; i++) {
+    coordinates[i][0] -= deltaX;
+    coordinates[i][1] -= deltaY;
+  }
+  const A = coordinates[1][1],
+    B = -coordinates[1][0];
+  for (let i = 2; i < n; i++) {
+    const [x, y] = [coordinates[i][0], coordinates[i][1]];
+    if (A * x + B * y !== 0) {
+      return false;
+    }
+  }
+  return true;
+};
