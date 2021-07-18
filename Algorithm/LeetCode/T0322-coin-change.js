1c6cd66cc51ec2d3d3978f510e0e5b7f95da292a
taujiong <taujiong@outlook.com>
2021-07-18 17:39:23 +0800

    feat(Algorithm): LeetCode T322 零钱兑换 中等

---
+++ Algorithm/LeetCode/T0322-coin-change.js
@@ -0,0 +1,20 @@
+/**
+ * https://leetcode-cn.com/problems/coin-change/
+ * 中等，动态规划
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  const dp = new Array(amount + 1).fill(Infinity);
+  dp[0] = 0;
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      if (coins[j] <= i) {
+        dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+      }
+    }
+  }
+
+  return dp[amount] > amount ? -1 : dp[amount];
+};
