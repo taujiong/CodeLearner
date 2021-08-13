bcecb0d06efed76e1782a62bf0d74cfc988688dd
taujiong <taujiong@outlook.com>
2021-08-13 09:41:27 +0800

    feat(code): 2021.8.13 algothrim exercise

---
+++ Algorithm/LeetCode/T0518-coin-change-ii.js
@@ -0,0 +1,17 @@
+/**https://leetcode-cn.com/problems/coin-change-2/
+ * 中等
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function (amount, coins) {
+  let dp = new Array(amount + 1).fill(0);
+  dp[0] = 1;
+  for (const coin of coins) {
+    for (let i = coin; i <= amount; i++) {
+      dp[i] += dp[i - coin];
+    }
+  }
+
+  return dp[amount];
+};
